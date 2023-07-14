9ecd9a1f910d52bf394a0215eaef1b46c7c86524
DaenerysTar <861066998@qq.com>
2023-07-14 11:05:55 +0800

    lab4

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -7,6 +7,23 @@
 #include "spinlock.h"
 #include "proc.h"
 
+uint64 
+sys_sigalarm(void) {
+  int n;
+  uint64 fn;
+  if(argint(0, &n) < 0)
+    return -1;
+  if(argaddr(1, &fn) < 0)
+    return -1;
+  
+  return sigalarm(n, (void(*)())(fn));
+}
+
+uint64 
+sys_sigreturn(void) {
+	return sigreturn();
+}
+
 uint64
 sys_exit(void)
 {
@@ -57,6 +74,8 @@ sys_sleep(void)
 {
   int n;
   uint ticks0;
+  
+  backtrace(); // print stack backtrace.
 
   if(argint(0, &n) < 0)
     return -1;
